99191e92027d8efd6d43d05ee8faaf5acdef1456
Goran <goranbubalo2422003@gmail.com>
2025-01-31 13:02:09 +0100

    Generics push

---
+++ Type parameter/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        IntegerPrinter printer = new IntegerPrinter(23);
+        printer.print();
+
+//        Printer<Integer> printer = new Printer<>(23);
+//        printer.print();
+
+//        Printer<String> printer1 = new Printer<>("Marko");
+//        printer1.print();
+//
+//        Printer<Double> doublePrinter = new Printer<>(23.5);
+//        doublePrinter.print();
+
+//        shout("John");
+//        shout(25);
+//        shout(26.7777);
+
+
+//        List<Integer> intList = new ArrayList<>();
+//        intList.add(4);
+//        printList(intList);
+
+    }
+    // Generic function
+//    private static <T> void shout(T thingsToShout) {
+//        System.out.println(thingsToShout + "!!!!!!");
+//    }
+
+//    private static void printList(List<Object> myList) {
+//        System.out.println(myList);
+//    }
+}
